7a12ab86ccc29222af9f988c6f26d54be97b79a2
Karina <kbrilhante@gmail.com>
2024-09-03 22:28:43 -0300

    Initial commit

---
+++ main.c
@@ -0,0 +1,88 @@
+#include <stdbool.h>
+#include <stdio.h>
+#define ex01
+
+/*** EXERCICIOS CONDICIONAIS ***/
+
+#ifdef ex01
+/*
+	DESAFIO: Escreva um programa que retorna se o ano é bissexto
+    Regras:
+    O ano bissexto se é
+    > divisivel por 4
+    > não é divisível por 100
+    > divisível por 400
+    
+    Exemplos:
+    1996 é bissexto
+    1845 não é bissexto
+    2023 é bissexto
+    1900 não é bissexto
+    2000 é bissexto
+*/
+int main(int argc, char **argv)
+{
+	printf("");
+    getchar();
+	return 0;
+}
+#endif
+
+#ifdef ex02
+/*
+	Hora do dia
+	Escreva um programa que cumprimeta o usuário de acordo com a hora do
+dia:
+
+	6:00 - 12:00: “Bom dia!”
+	12:00 - 18:00: “Boa Tarde!”
+	18:00 - 00:00: “Boa Noite!”
+	00:00 - 6:00: “Por que você tá acordado?”
+*/
+int main(int argc, char **argv)
+{
+	printf("");
+    getchar();
+	return 0;
+}
+#endif
+
+#ifdef ex03
+/*
+	Elabore um algoritmo que calcule o valor a ser pago de uma mercadoria 
+	+------------------+-------------------+
+	|  Pagameento      | Desconto          |
+	|  Dinheiro        | 15%               | 
+	|  Crédito a vista | 10%               | 
+	|  Parcelado       | Preço da etiqueta |
+	+------------------+-------------------+
+*/
+int main(int argc, char **argv)
+{
+	printf("");
+    getchar();
+	return 0;
+}
+#endif
+
+#ifdef ex04
+/*
+	Média e conceito
+	Escreva um programa que calcula a média das 4 notas de um aluno e retorna o conceito de acordo com a tabela
+	+-----------------+----------+
+	| Média           | Conceito |
+	+-----------------+----------+
+	| acima de 90%    |     A    |
+	| entre 75% e 90% |     B    | 
+	| entre 60% e 75% |     C    | 
+	| entre 40% e 60% |     D    | 
+	| abaixo de 40%   |     E    |
+	+-----------------+----------+
+*/
+int main(int argc, char **argv)
+{
+	printf("");
+    getchar();
+	return 0;
+}
+#endif
